ed1cbcbe72a1f640cc0efa4202a294432b6ba912
pedro-candido <pedroferreirac@hotmail.com>
2022-06-18 07:14:44 -0300

    feat: react-icons added

--- src/components/Comment/Comment.styles.ts
+++ src/components/Comment/Comment.styles.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
+import { FiTrash } from "react-icons/fi";
 
 const Container = styled.main``;
 
 const Header = styled.div``;
+
+export const Trash = styled(FiTrash)``;
 
 export const Author = {
   Name: styled.p``,
--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -45,3 +45,5 @@ const Comment = ({
     </>
   );
 };
+
+export default Comment;
